04ec2dde752b1182faeea0c14e5972d9e1588e62
Marcelo Pereira <marcelopereiraIFSP@gmail.com>
2018-08-01 18:51:45 -0300

    Created migrations roles, users_has_roles and permissions

--- database/migrations/2018_08_01_204651_posts.php
+++ database/migrations/2018_08_01_204651_posts.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class Posts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create("posts", function(Blueprint $table){
+            $table->increments("id");
             $table->string("title", 50);
             $table->text("description");
             $table->integer("user_id")->unsigned();
             $table->timestamps();
 
-            $table->foreign("user_id")->references("id")->on("users")->onDelete("CASCADE");
+            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
         });
     }
 
@@ -31,6 +31,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists("posts");
     }
 }
